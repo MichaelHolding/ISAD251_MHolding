a904f44571d04c231736f780289e9cd6b16f7452
MichaelHolding <michael.holding@students.plymouth.ac.uk>
2020-01-08 14:05:04 +0000

    Edited index.php

--- public/index.php
+++ public/index.php
@@ -10,10 +10,10 @@ include_once "header.php"
     <div class="w3-col m6 w3-padding-large w3-hide-small"
     </div>
 
-    <div class="w3-col m6 w3-padding-large">
+    <div class="w3-col m16 w3-padding-large">
       <h1 class="w3-center">About Le Pub</h1><br>
       <h5 class="w3-center">Tradition since 1889</h5>
-      <p class="w3-large">The Catering was founded in blabla by Mr. Smith in lorem ipsum dolor sit amet, consectetur adipiscing elit consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute iruredolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.We only use <span class="w3-tag w3-light-grey">seasonal</span> ingredients.</p>
+      <p class="w3-large">Le Pub was founded to share with the community the smaller local beers and ciders as well as showcasing classic english pub food along with groundbreaking dishes from the continent. <span class="w3-tag w3-light-grey">seasonal</span> ingredients.</p>
     </div>
   </div>
 
@@ -27,7 +27,7 @@ include_once "header.php"
     </div>
 
     <div class="w3-col l6 w3-padding-large">
-      <img src="/w3images/tablesetting.jpg" class="w3-round w3-image w3-opacity-min" alt="Menu" style="width:100%">
+
     </div>
   </div>
 
